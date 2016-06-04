b0b817a2ca95dcfa2bc6cd88032385ed540418be
Alessandro Tundo <alessandrotundo94@gmail.com>
2016-06-04 17:07:30 +0200

    Added templates, static files and configs
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- texla_api.py
+++ texla_api.py
@@ -1,20 +1,17 @@
-import texla_core.log
-from flask import Flask, request, jsonify
+import log
+from flask import Flask, request, jsonify, render_template
 import json
 import os, random, string
-from texla_core.texla.Parser import Parser
-from texla_core.texla.Renderers.MediaWikiRenderer import MediaWikiRenderer
 
-app = Flask('texla_api')
-app.secret_key = 'abcdef'
+app = Flask('texla_api', instance_relative_config=True)
+# Load the default configuration
+app.config.from_object('config')
+# Load the configuration from the instance folder
+app.config.from_pyfile('config.py')
 
 @app.route('/')
 def welcome():
-    return "Welcome to TeXLa!"
-
-@app.route('/texla')
-def helloworld():
-    return "Start to import :)"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-        app.run(host="127.0.0.1", port=5000, debug=True)
+        app.run(host="127.0.0.1", port=5000)
