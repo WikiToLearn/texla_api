94523b43ee13ffac4ef7e6db24ad896e98ba9f6c
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-06-04 16:58:43 +0200

    First commit for texla_api

---
+++ texla_api.py
@@ -0,0 +1,20 @@
+import texla_core.log
+from flask import Flask, request, jsonify
+import json
+import os, random, string
+from texla_core.texla.Parser import Parser
+from texla_core.texla.Renderers.MediaWikiRenderer import MediaWikiRenderer
+
+app = Flask('texla_api')
+app.secret_key = 'abcdef'
+
+@app.route('/')
+def welcome():
+    return "Welcome to TeXLa!"
+
+@app.route('/texla')
+def helloworld():
+    return "Start to import :)"
+
+if __name__ == '__main__':
+        app.run(host="127.0.0.1", port=5000, debug=True)
