95d4fcf9d1cd32c330e894ee6a2bc523b15f083f
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-06-04 17:30:10 +0200

    Added texla as submodule

--- texla/Parser/Blocks/Utilities/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import CommandParser
-from . import EnvironmentParser
-from . import utility
-from . import MacroParser
--- texla_api.py
+++ texla_api.py
@@ -2,14 +2,14 @@ import log
 from flask import Flask, request, jsonify, render_template
 import json
 import os, random, string
-from texla.Parser import Parser
-from texla.Renderers.MediaWikiRenderer import MediaWikiRenderer
+from texla_lib.texla.Parser import Parser
+from texla_lib.texla.Renderers.MediaWikiRenderer import MediaWikiRenderer
 
 app = Flask('texla_api', instance_relative_config=True)
 # Load the default configuration
 app.config.from_object('config')
 # Load the configuration from the instance folder
-app.config.from_pyfile('config.py')
+#app.config.from_pyfile('config.py')
 
 @app.route('/')
 def welcome():
