aace95b2531e73ad0fc15a70920485ec96e9da08
Alessandro Tundo <alessandrotundo94@gmail.com>
2016-08-16 01:03:50 +0200

    Improved start-service script and uptated submodule
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- start-service.sh
+++ start-service.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-
-docker run -ti \
-        --name texla-api \
-        -p 80:80 \
-        wikitolearn/texla-api
+set -x
+set -e
+docker stop texla-api
+docker rm texla-api
+git pull origin master
+git checkout master
+docker build -t wikitolearn/texla-api --rm .
+docker run \
+  --name texla-api \
+  -p 80:80 \
+  --restart=always \
+  -d wikitolearn/texla-api
