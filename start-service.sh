94f9957d22af062c7ddd95d4584d32b98d05a60b
Toma Luca <toma.luca95@gmail.com>
2016-09-09 09:00:11 +0200

    some docker fixes
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- start-service.sh
+++ start-service.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -x
 set -e
-docker stop texla-api
-docker rm texla-api
+if [[ $(docker ps -q --filter name=texla-api$ | wc -l) -gt 0 ]]
+then
+    docker stop texla-api
+fi
+
+if [[ $(docker ps -aq --filter name=texla-api$ | wc -l) -gt 0 ]]
+then
+    docker rm texla-api
+fi
+
 git pull origin master
 git checkout master
+
 docker build -t wikitolearn/texla-api --rm .
 docker run \
   --name texla-api \
