677778fb676804d85759d05b1470f55296036a28
Davide Valsecchi <valsecchi.davide94@gmail.com>
2017-03-09 11:01:34 +0100

    Updated texla and added error message

--- texla_helper.py
+++ texla_helper.py
@@ -8,34 +8,37 @@ import texla_lib.texla.PageTree.Exporter as exporter
 
 
 def convert_text_simple(text, lang):
-    '''This function convert a tex document in a single
-    wikitext output collapsing the content at the  -1 level'''
-    config = {"input_path":"",
-              "output_path":"",
-              "doc_title":"texla_min",
-              "lang":lang,
-              "keywords":json.loads(
-                  open('lang.txt').read())[lang],
-              "collapse_content_level":-1,
-              "base_path":'',
-              "create_index":0,
-              "plugins": ["MathCheck", "spaces_check"]
-              }
-    logging.info('######## STARTING PARSING ########')
-    p = Parser(config)
-    tree = p.parse(text)
-    n_blocks = tree.n_blocks()
-    logging.info('PARSED %i Blocks', n_blocks)
+    try:
+        '''This function convert a tex document in a single
+        wikitext output collapsing the content at the  -1 level'''
+        config = {"input_path":"",
+                  "output_path":"",
+                  "doc_title":"texla_min",
+                  "lang":lang,
+                  "keywords":json.loads(
+                      open('lang.txt').read())[lang],
+                  "collapse_content_level":-1,
+                  "base_path":'',
+                  "create_index":0,
+                  "plugins": ["MathCheck", "spaces_check"]
+                  }
+        logging.info('######## STARTING PARSING ########')
+        p = Parser(config)
+        tree = p.parse(text)
+        n_blocks = tree.n_blocks()
+        logging.info('PARSED %i Blocks', n_blocks)
 
-    logging.info('######## STARTING RENDERING ########')
-    #rendering
-    rend = MediaWikiRenderer(config)
-    rend.start_rendering(tree)
-    #collpasing tree
-    tree = rend.tree
-    rend.tree.collapse_tree(config['collapse_content_level'], 4)
-    #collapse the text in the right pages
-    #getting text of root_page
-    output_text = rend.tree.root_page.text
-    logging.info('######## FINISHED ########')
+        logging.info('######## STARTING RENDERING ########')
+        #rendering
+        rend = MediaWikiRenderer(config)
+        rend.start_rendering(tree)
+        #collpasing tree
+        tree = rend.tree
+        rend.tree.collapse_tree(config['collapse_content_level'], 4)
+        #collapse the text in the right pages
+        #getting text of root_page
+        output_text = rend.tree.root_page.text
+        logging.info('######## FINISHED ########')
+    except:
+        return "Some errors in your Latex file!"
     return output_text.strip()
